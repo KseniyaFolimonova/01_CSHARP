c2801778f3ff975984759d814c7067f91a4406cb
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-01-29 21:47:19 +0300

    add Tasks in Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -11,3 +11,59 @@ int LastDigit = number % 10;
 
 System.Console.WriteLine(FirstDigit + LastDigit);
 */
+/*
+     int firstNumber = 3;
+     int secondNumber = 3;
+
+     if (firstNumber < secondNumber)
+     {
+         Console.WriteLine("второе число больше");
+     }
+     else if (firstNumber > secondNumber)
+     {
+         Console.WriteLine("первое число больше");
+     }
+     else
+     {
+         System.Console.WriteLine("числа равны");
+     }
+     */
+/*
+int a = 5;
+int b = 6;
+int c = 7;
+int FindMax = 0;
+
+if (a > FindMax)
+{
+    FindMax = a;
+}
+else if (b > FindMax)
+{
+    FindMax = b;
+}
+else if (c > FindMax)
+{
+    FindMax = c;
+}
+return (FindMax);
+
+
+        int number = 3;
+        if (number % 2 == 0)
+        {
+            System.Console.WriteLine("четное");
+        }
+        else
+        {
+            System.Console.WriteLine("нечетное");
+        }
+*/
+        int number = 8;
+        int i = 1;
+        while (i <= number)
+        {
+            if (i % 2 == 0)
+                Console.Write(i + " ");
+            i += 1;
+        }
