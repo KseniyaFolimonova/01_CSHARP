f4000237baba3589d5120c5fb9a81d6cc727bcfd
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-01-29 20:10:51 +0300

    add comments on Task8

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -4,8 +4,10 @@
 //456 => 10
 //782 => 9
 //918 => 17
-
+/*
 int number = 789;
-System.Console.WriteLine(number / 10);
-System.Console.WriteLine(number / 9);
-System.Console.WriteLine(number / 17);
+int FirstDigit = number /100;
+int LastDigit = number % 10;
+
+System.Console.WriteLine(FirstDigit + LastDigit);
+*/
