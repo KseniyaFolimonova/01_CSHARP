abc3938651a562fa4917d139593920d2cbdd98a2
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-01-30 19:22:50 +0300

    add arrya:Arr_Task1

---
+++ ARRAY/Arr_Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 8;
+arr[3] = 1;
+arr[4] = 2;
+System.Console.WriteLine(arr[0]);
+System.Console.WriteLine(arr[1]);
+System.Console.WriteLine(arr[2]);
+System.Console.WriteLine(arr[3]);
+System.Console.WriteLine(arr[4]);
+
+
+int[] arr2 = new int[5] { 1, 3, 5, 8, 0 };
+int[] arr3 = { 4, 8, 5, 7, 1 }
