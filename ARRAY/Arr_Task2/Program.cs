780f01fcf5796ec878b97088e431289106248a53
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-03 10:18:00 +0300

    add new task to the  Arr_Task2

---
+++ ARRAY/Arr_Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿//int n = 10;
+int[] arr = { 2, 5, 7, 8, 10, 12, 3, 0, 48, 97 };
+int i = 0;
+
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+       
+    }
+    i = i + 1;
+}
