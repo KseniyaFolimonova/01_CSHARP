81ef772a03c845e5bcac37518d174329828a1844
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-03 10:29:36 +0300

    add new file Arr_Tak3

---
+++ ARRAY/Arr_Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = 5;
+int[] arr = { 8, 0, 45, 28, 63 };
+int i = 0;
+int max = arr[0];
+
+while (i < n)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    i = i +1;
+}
+System.Console.WriteLine(max);
