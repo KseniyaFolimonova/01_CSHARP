2725cf7286baab5f9504730023da7804aa890755
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-03 08:53:46 +0300

    add new task for array to the Arr_Task1

--- ARRAY/Arr_Task1/Program.cs
+++ ARRAY/Arr_Task1/Program.cs
@@ -15,10 +15,14 @@ int[] arr2 = new int[5] { 1, 3, 5, 8, 0 };
 int[] arr3 = { 4, 8, 5, 7, 1 }
 */
 int n =10;
-int[] Arr = new int[n];
+int[] arr = new int[n];
 int i =0;
 
 while (i < n)
 {
     arr[i] = i + 1;
+   // Console.Write(Arr[i]);
+   // Console.Write(" ");
+    Console.Write($"{arr[i]} ");
+    i = i + 1;
 }
