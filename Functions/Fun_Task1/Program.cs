4a379de1eda8b76ce587bd915c1e3769b6857884
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-05 21:53:36 +0300

    add a new folder:Function

---
+++ Functions/Fun_Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculeteFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+
+}
+double result = CalculeteFormula(1, 2, 3, 4);
+System.Console.WriteLine(result);
