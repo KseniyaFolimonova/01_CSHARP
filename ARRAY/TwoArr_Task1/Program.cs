e9e5912857e7e75e3b23c519828a5478664ef6d0
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-11 09:36:56 +0300

    Add new file:TwoArr_Task1

---
+++ ARRAY/TwoArr_Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+int[,] CreateMatrix(int rowCount, int columCount)
+{
+    int[,] matrix = new int[rowCount, columCount];
+    Random rnd = new Random();
+
+    // Заполнение матрицы случайными числами от 1 до 10
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 11);
+        }
+
+    }
+    return matrix;
+}
+// Вывод матрицы
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine(); // Переход на новую строку после каждой строки матрицы
+    }
+}
+int[,] matrix = CreateMatrix(4, 5);
+ShowMatrix(matrix);
