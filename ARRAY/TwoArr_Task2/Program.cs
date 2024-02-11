521b6a76637a580317a1eb2da17ded31b49d1f8f
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-11 12:18:56 +0300

    update TwoArr_Task2

--- ARRAY/TwoArr_Task2/Program.cs
+++ ARRAY/TwoArr_Task2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateArray(int rowCount, int columCount)
+{
+    int[,] array = new int[rowCount, columCount];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); i++)
+        {
+            array[i, j] = rnd.Next(1, 1000)
+        }
+    }
+    return array;
+}
+int[,] array = CreateArray(2,4);
