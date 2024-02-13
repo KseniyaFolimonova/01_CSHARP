a436d08cc8407ae41f3ae495570085768e8c61bb
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-13 22:11:45 +0300

    update TwoArr_Task2

--- ARRAY/TwoArr_Task2/Program.cs
+++ ARRAY/TwoArr_Task2/Program.cs
@@ -1,15 +1,67 @@
-﻿int[,] CreateArray(int rowCount, int columCount)
-{
-    int[,] array = new int[rowCount, columCount];
-    Random rnd = new Random();
+﻿﻿// Назовем число “интересным” если его сумма цифр четная
+// Создавать двумерный массив, состоящий из целых чисел. 
+//Вывести на экран “интересные” элементы массива
 
-    for (int i = 0; i < array.GetLength(0); i++)
+int[,] CreateMatrix (int rowCount, int colomsCount)
+{
+int[,] matrix = new int[rowCount,colomsCount];
+Random rnd = new Random();
+for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
     {
-        for (int j = 0; j < array.GetLength(1); i++)
-        {
-            array[i, j] = rnd.Next(1, 1000)
-        }
+        matrix[i,j] = rnd.Next(1, 1000);
     }
-    return array;
 }
-int[,] array = CreateArray(2,4);
+return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+{
+    for(int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.Write($"{matrix[i,j]} "); // такой вывод называется интерполяция строк
+    }
+    System.Console.WriteLine();
+}
+}
+
+
+
+int[,] matrix = CreateMatrix(3, 4);
+
+ShowMatrix(matrix);
+
+foreach (int e in matrix)
+{
+    if(IsInteresting(e) == true)
+    {
+        System.Console.WriteLine(e);
+    }
+}
+
+bool IsInteresting(int value)
+{
+    int SumOfDigits = GetSumOfDigits(value);
+
+    if(SumOfDigits % 2 == 0)
+    {
+        return true;
+    }
+    //else{   сокращение кода
+        return false;
+   // }
+}
+
+int GetSumOfDigits (int value)
+{
+    int sum = 0; 
+    while(value > 0)
+    {
+        sum = sum + value % 10; // получаем самое правое число
+        value = value / 10;
+    }
+    return sum;
+}
