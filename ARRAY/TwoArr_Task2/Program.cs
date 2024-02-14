86de981bd5deca0f28df7a74d521b4292d231ebd
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-14 22:34:08 +0300

    new TwoArr_Task2

--- ARRAY/TwoArr_Task2/Program.cs
+++ ARRAY/TwoArr_Task2/Program.cs
@@ -1,67 +1,19 @@
-﻿﻿// Назовем число “интересным” если его сумма цифр четная
-// Создавать двумерный массив, состоящий из целых чисел. 
-//Вывести на экран “интересные” элементы массива
+﻿﻿﻿// Считать с консоли строку, состоящую из цифр и латинских букв.
+//Сформировать новую строку, состоящую из букв исходной строки.
 
-int[,] CreateMatrix (int rowCount, int colomsCount)
+string GetLettersFromString(string s)
 {
-int[,] matrix = new int[rowCount,colomsCount];
-Random rnd = new Random();
-for(int i = 0; i < matrix.GetLength(0); i++)
-{
-    for(int j = 0; j < matrix.GetLength(1); j++)
+    string letters = "";
+    foreach(char e in s)
     {
-        matrix[i,j] = rnd.Next(1, 1000);
+        if(char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
     }
-}
-return matrix;
-}
-
-void ShowMatrix(int[,] matrix)
-{
-    for(int i = 0; i < matrix.GetLength(0); i++)
-{
-    for(int j = 0; j < matrix.GetLength(1); j++)
-    {
-        System.Console.Write($"{matrix[i,j]} "); // такой вывод называется интерполяция строк
-    }
-    System.Console.WriteLine();
-}
+    return letters;
 }
 
-
-
-int[,] matrix = CreateMatrix(3, 4);
-
-ShowMatrix(matrix);
-
-foreach (int e in matrix)
-{
-    if(IsInteresting(e) == true)
-    {
-        System.Console.WriteLine(e);
-    }
-}
-
-bool IsInteresting(int value)
-{
-    int SumOfDigits = GetSumOfDigits(value);
-
-    if(SumOfDigits % 2 == 0)
-    {
-        return true;
-    }
-    //else{   сокращение кода
-        return false;
-   // }
-}
-
-int GetSumOfDigits(int value)
-{
-    int sum = 0; 
-    while(value > 0)
-    {
-        sum = sum + value % 10; // получаем самое правое число
-        value = value / 10;
-    }
-    return sum;
-}
+string str = Console.ReadLine();
+string result = GetLettersFromString(str);
+System.Console.WriteLine(result);
