b047c0397e71708b30c64ecf13aa98e0bbf95696
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-14 22:35:04 +0300

    update TwoArr_Task2.1

--- ARRAY/TwoArr_Task2.1/Program.cs
+++ ARRAY/TwoArr_Task2.1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string ExtractDigits(string input)
+{
+    SrtingBuilder digits  = new SrtingBuilder(); //создает новый объект типа StringBuilder, который будет использоваться для построения строки, содержащей цифры из введенной строки.
+    foreach(char character in input)
+    {
+        if(char.IsDigit(character))
+        {
+            digits.Append(character);
+        }
+    }
+    return digits.ToString();
+}
+
+
+Console.Write("Введите чтроку с числами и символами:");
+string inputString = Console.ReadLine();
+System.Console.Write(ExtractDigits(inputString));
+
