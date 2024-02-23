b8a02b3d2c97794b33c610af19bc7a8baa3aa216
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-23 21:32:04 +0300

    add new file on RECURSION:Task1

---
+++ RECURSION/Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+void OpenMatryoshka(int size)
+{
+    if(size == 1)
+    {
+        return;
+    }
+    OpenMatryoshka(size - 1);
+    System.Console.WriteLine($"Opening Matryoshka of size {size}...");
+    
+}
+OpenMatryoshka(5);
+*/
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        System.Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    System.Console.WriteLine(n);
+    return n * Fact(n-1);
+}
+System.Console.WriteLine(Fact(5));
+// F11  шаг с заходом 
+// F10 шаг с обходом
+// F5 продолжить (до следующей точки)
