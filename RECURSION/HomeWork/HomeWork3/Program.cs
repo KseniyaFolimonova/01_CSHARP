58a9c53b563cdb25e04acec49e5c1d3e4085e147
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-25 19:29:25 +0300

    update RECURSION:HomeWork3

--- RECURSION/HomeWork/HomeWork3/Program.cs
+++ RECURSION/HomeWork/HomeWork3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 3: Задайте произвольный массив. Выведете его элементы, 
+начиная с конца. Использовать рекурсию, не использовать циклы.
+*/
+void ArrayReversed(int[] array, int i)
+{
+    if (i >= 0)
+    {
+        System.Console.Write(array[i] + " ");
+        ArrayReversed(array, i - 1);
+    }
+}
+
+int[] array = { 1, 2, 3, 4, 5};
+
+System.Console.WriteLine("Реверсивный массив");
+ArrayReversed(array, array.Length - 1);
