16da1165ee98f5046d2f5a2487587ca249a6135c
Kseniya Folimonova <kseniyafolimonova@tandex.ru>
2024-02-25 12:00:07 +0300

    add new fail:Task4

---
+++ RECURSION/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задание 4.
+Считать строку с консоли, содержащую латинские буквы.
+Вывести на экран согласные буквы этой строки.
+*/
+bool IsLetter(char ch)
+{
+    return (('a' <= ch && 'z' >= ch) || ('A' <= ch && 'Z' >= ch));
+}
+
+bool IsVowels(char ch)
+{
+    string vowels = "aeyuioAEYUIO";
+    for (int i = 0; i < vowels.Length; i++)
+    {
+        if (ch == vowels[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void PrintConsonants(string word, int i = 0)
+{
+    if (i >= word.Length)
+    {
+        return;
+    }
+
+    //string vowels = "aeyuio";
+    //if (char.IsLetter(word[i]) && !vowels.Contains(word[i]))
+    if (IsLetter(word[i]) && !IsVowels(word[i]))
+    {
+        System.Console.Write(word[i] + " ");
+    }
+    PrintConsonants(word, i + 1);
+}
+
+PrintConsonants("kjhfqeihj4nb;nHGIUCVIYCI");
